927741d25532f76d5859580bd2b1f76923c15a70
MrFlinck <114952688+MrFlinck@users.noreply.github.com>
2024-02-04 16:27:08 -0300

    tentativa de adicionar

---
+++ CSAPP/Program.cs
@@ -0,0 +1,30 @@
+﻿using System; 
+using System.Globalization; 
+using System.Collections.Generic; 
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CSAPP
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Teste x; 
+            x = new Teste(); 
+            /// obs: por algum motivo que não sei, o terminal integrado funciona mas o externo e interno não funciona...
+
+
+            Console.Write("Digite o primeiro número: "); 
+            x.Num = int.Parse(Console.ReadLine()); 
+            Console.Write("Digite o segundo número: "); 
+            x.Num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("A soma dos números é: {0} " , x.Soma(x.Num , x.Num2)); 
+
+
+
+
+        }
+    }
+}
---
+++ CSAPP/Program2.cs
@@ -0,0 +1,17 @@
+using System; 
+
+public class Teste{
+
+    public int Num; 
+    public int Num2; 
+
+
+    public int Soma(int num, int num2 ){
+
+        Num = num; 
+        Num2 = num2; 
+        return Num + Num2 ; 
+
+
+    }
+}
